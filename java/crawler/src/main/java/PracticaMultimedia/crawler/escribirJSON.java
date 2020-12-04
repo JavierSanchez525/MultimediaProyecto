850a6b679798b8eacb19de0a77e877004c81e438
Ruben5914 <58144362+Ruben5914@users.noreply.github.com>
2020-12-04 10:57:45 +0100

    Add files via upload

---
+++ java/crawler/src/main/java/PracticaMultimedia/crawler/escribirJSON.java
@@ -0,0 +1,56 @@
+package PracticaMultimedia.crawler;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import org.jsoup.select.Elements;
+
+
+
+public class escribirJSON {
+
+	
+	public static String getTitulo (Document doc) throws IOException {
+		String titulo = doc.select("div.title_wrapper").select("h1").text().replace('(', '-').split("-")[0];
+		return titulo;
+	}
+	
+	public static int getAño (Document doc) throws IOException {
+		int año = Integer.parseInt(doc.select("div.title_wrapper").select("h1").text().replace('(', '-').split("-")[1].replace(")", ""));
+		return año;
+	}
+	
+	public static String[] getGenerosYKeywords (Document doc) throws IOException { 
+		//devuelve en la posicion 0 los generos y en la 1 las keywords
+		//(se extraen ambos atributos en el mismo metodo para optimizar la extracción)
+		String [] GenerosKeywords = new String [2];
+		Elements datos = doc.select("div#titleStoryLine");
+		
+		//Primero obtengo los generos (SOLO OBTIENE LOS GENEROS DE LA PAGINA EN CASO DE "SEE MORE" HAY QUE CAMBIAR CODIGO)
+		GenerosKeywords[0] = datos.select("h4:contains(Genres) ~ a").text();
+		
+		
+		String urlKeyWords = datos.select("h4:contains(Plot Keywords:) ~ nobr").select("a:contains(See All)").attr("abs:href");
+		
+		Document paginaKeyWords =  Jsoup.connect(urlKeyWords).get();
+		// (CUIDADO YA QUE AQUELLOS KEYWORDS QUE SEAN COMPUESTOS ES DECIR QUE SEAN MAS DE UNA PALABRA SE SEPARARAN) 
+		GenerosKeywords[1] = paginaKeyWords.select("tbody").select("div.sodatext").select("a").text();
+		//select("tbody").select("div.sodatext").
+		
+		return GenerosKeywords;
+	}
+	
+	public static String getActores (Document doc) throws IOException { 
+		
+		
+		Elements datos = doc.select("div#titleCast");
+		String urlActores = datos.select("div.see-more").select("a:contains(See full cast)").attr("abs:href");
+		Document paginaCast =  Jsoup.connect(urlActores).get();
+		String actores = paginaCast.select("table.cast_list").select("tbody").select("tr.odd, tr.even").select("td:not(td.ellipsis)").select(" td:not(td.character)").text();
+		
+		
+		return actores;
+	}
+}
---
+++ java/crawler/src/main/java/PracticaMultimedia/crawler/leerExcel.java
@@ -0,0 +1,71 @@
+package PracticaMultimedia.crawler;
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+
+public class leerExcel {
+
+	public static String[] leerURLs() throws IOException, InvalidFormatException {
+        OPCPackage pkg = OPCPackage.open(new File("src\\MovieGenreIGC_v3.xlsx"));
+        XSSFWorkbook wb = new XSSFWorkbook(pkg);
+        XSSFSheet sheet = wb.getSheetAt(0);
+        int rows = sheet.getLastRowNum();
+        String[] urls = new String[rows];
+        
+        for (int i = 1; i <= 6/*rows*/; i++) {
+        	XSSFRow row = sheet.getRow(i);
+        	
+        	XSSFCell url = row.getCell(1);
+        	String titulo_url = url.getStringCellValue();
+        	urls[i-1] = titulo_url+'/';
+        	
+        }
+        wb.close();
+        return urls;
+    }
+	
+	public static void main(String[] args) throws IOException, InvalidFormatException{
+		String[] pelicula = leerURLs();
+		for (int i = 0; i < 5; i++) {
+			
+	        JSONObject myObject = new JSONObject();
+	        
+	        Document doc = Jsoup.connect(pelicula[i]).get();
+	        /*
+	        // Sacamos el titulo
+	        
+	        myObject.put("titulo",  );
+	        
+	        // Sacamos el año
+	        myObject.put("año", );
+
+			*/
+			String titulo = escribirJSON.getTitulo(doc);
+			int año = escribirJSON.getAño(doc);
+			String [] GenerosYKeyWords = new String [2];
+			GenerosYKeyWords = escribirJSON.getGenerosYKeywords(doc);
+			String actores = escribirJSON.getActores(doc);
+			System.out.println(pelicula[i]);
+			System.out.println(titulo);
+			System.out.println(año);
+			System.out.println(GenerosYKeyWords[0]);
+			System.out.println(GenerosYKeyWords[1]);
+			System.out.println(actores);
+			System.out.println();
+			System.out.println();
+			
+		}	
+		
+		
+	}
+}
